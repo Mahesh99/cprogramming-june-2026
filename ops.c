4ebc1bd271ec11e5f0ede3417d8ade60974a7339
Mahesh Arya <mahesharya@prmanicus.com>
2026-06-19 17:52:43 +0530

    c

---
+++ ops.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+    int a;
+    short b;
+    float c;
+    double d;
+     printf(" %d",6&3); 
+     printf(" %d",110<<2);
+     printf(" %d",sizeof a);
+     printf(" %d",sizeof b);
+     printf(" %d",sizeof c);
+     printf(" %d",sizeof d);
+}
